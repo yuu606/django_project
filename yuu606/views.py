bfbeba71714287e76e6a1ea9a603ec964eed5b9e
yuu606 <yuukinori606@gmail.com>
2023-03-06 16:41:26 +0800

    updates

--- yuu606/views.py
+++ yuu606/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+    'author': 'yuu', 
+    'title': 'Blog Post 1', 
+    'content': 'First post content', 
+    'date_posted': 'March 23, 2023'
+    }, 
+    {
+    'author': 'Jane Doe', 
+    'title': 'Blog Post 2', 
+    'content': 'First post content', 
+    'date_posted': 'March 24, 2023'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
