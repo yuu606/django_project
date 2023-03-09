32b7e29dcb76e61a91e10996b5f7764c35abfe02
yuu606 <yuukinori606@gmail.com>
2023-03-10 02:49:02 +0800

    users app

--- yuu606/views.py
+++ yuu606/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-    'author': 'yuu', 
-    'title': 'Blog Post 1', 
-    'content': 'First post content', 
-    'date_posted': 'March 23, 2023'
-    }, 
-    {
-    'author': 'Jane Doe', 
-    'title': 'Blog Post 2', 
-    'content': 'First post content', 
-    'date_posted': 'March 24, 2023'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
